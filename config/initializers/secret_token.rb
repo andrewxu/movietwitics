12474311b4481ee9cc96b0a571a97e01b7f2c73b
Andrew Xu <andrewxu0920@gmail.com>
2012-11-03 12:49:45 -0600

    Initial app commit
    Adding the rails app structure, originated from paul's repo git://github.com/paulb896/cool_new_project.git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = 'fc8fd1718198090da0553afeb42bdcca9b4a86e416c7c05334ad5b057bcf38c22106259f6ebaa544bb35c6e709abdbe002efe34d6d4e587aac2bd0a118707449'
